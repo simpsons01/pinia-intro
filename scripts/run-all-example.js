340156747033f5166e0ef2feab610d9c01d39fcb
simpsosns01 <ilikekobe01@gmail.com>
2023-12-20 11:58:10 +0800

    update cmd

--- scripts/run-all-example.js
+++ scripts/run-all-example.js
@@ -9,17 +9,19 @@ class PageRunner {
     this._spawnProcess = spawn(cmd, arg, { cwd: join(__dirname, "..") }) 
 
     this._spawnProcess.stdout.on('data', (data) => {
-       console.log(`from cmd: "${cmd}  ${arg.join(" ")} stdout"`)
+       console.log(`cmd: "${cmd}  ${arg.join(" ")} stdout"`)
        console.log(data.toString())
     });
     
-    this._spawnProcess.stderr.once('data', (data) => {
+    this._spawnProcess.stderr.on('data', (data) => {
       this._spawnProcess = null
-      console.error(`stderr: ${data}`);
+      console.log(`cmd: "${cmd}  ${arg.join(" ")} stderr"`)
+      console.error(data.toString());
     });
     
     this._spawnProcess.once('close', (code) => {
       this._spawnProcess = null
+      console.log(`cmd: "${cmd}  ${arg.join(" ")} close"`)
       console.log(`child process exited with code ${code}`);
     }); 
   }
@@ -29,6 +31,7 @@ const excludeCmd = [
   "run-all",
   "run-all-example"
 ]
+
 Object.keys(require(join(__dirname, "../package.json")).scripts).forEach(cmd => {
   if(excludeCmd.indexOf(cmd) === -1) {
     new PageRunner().run("pnpm", [cmd]) 
